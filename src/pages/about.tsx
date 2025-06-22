c33c10f1dc2c75dec8a012013800d8e73541c0ed
Arkar <arkarchanmyaeko@gmail.com>
2025-06-22 16:23:48 +0700

    wip(about): add name, contact informations and about me

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,48 @@
-import { Box } from '@chakra-ui/react';
+import { Box, IconButton, Clipboard } from '@chakra-ui/react';
 import React from 'react';
+import '@/styles/about.css';
+import data from '../../data.json';
+
+interface SocialLinks {
+  logo: string;
+  link: string;
+}
 
 const About: React.FC = () => {
   return (
     <>
-      <Box color='primary'>
-        About Page
+      <Box color="secondary" className="wrapper">
+        <Box className="name"> {data.name}</Box>
+        <Box className="title"> {data.title}</Box>
+        <Box className="email">
+          <Box>{data.email}</Box>
+          <Box className="icon">
+            <Clipboard.Root value={data.email}>
+              <Clipboard.Trigger asChild>
+                <IconButton variant="surface" size="2xs">
+                  <Clipboard.Indicator />
+                </IconButton>
+              </Clipboard.Trigger>
+            </Clipboard.Root>
+          </Box>
+        </Box>
+        <Box className="social-links">
+          {data.social_links.map((link: SocialLinks, index: number) => (
+            <Box key={index}>
+              <a href={link.link} target="_blank" rel="noopener noreferrer">
+                <img src={link.logo} alt={link.link} width={30} height={30} />
+              </a>
+            </Box>
+          ))}
+        </Box>
+        <Box className="about-me">
+          {data.about_me.map((text: string, index: number) => (
+            <Box key={index}>{text}</Box>
+          ))}
+        </Box>
       </Box>
     </>
   );
 };
 
-export default About; 
+export default About;
