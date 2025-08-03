6f70cdba98cbb6825ed5fa6446f4e7cf7b50850d
Arkar <arkarchanmyaeko@gmail.com>
2025-08-03 16:17:14 +0700

    add portfolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,13 +1,122 @@
 import React from 'react';
+import { Box, Card, Tag, Badge } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+import { TbWorld } from 'react-icons/tb';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import ImageGallery, { type ReactImageGalleryItem } from 'react-image-gallery';
 
-const Portfolio: React.FC = () => {
-  return (
-    <>
-      <div>
-        Portfolio Page
-      </div>
-    </>
-  )
+import data from '../../data/portfolio.json';
+import '@/styles/portfolio.css';
+
+type Project = {
+  title: string;
+  description: string;
+  tech_stacks: string[];
+  github?: Github[];
+  website?: string;
+  images?: {
+    original: string;
+  }[];
 };
 
-export default Portfolio; 
+type Github = {
+  label: string;
+  link: string;
+};
+const Portfolio: React.FC = () => {
+  const handleRedirect = (url: string) => {
+    window.open(url, '_blank');
+  };
+
+  return (
+    <>
+      <Box className="portfolio-wrapper">
+        <Box className="portfolio-sub-wrapper">
+          {data.projects.map((project: Project, index: number) => (
+            <Card.Root
+              overflow="hidden"
+              bg={'cardBackground'}
+              key={index}
+              className="card-container"
+            >
+              {/* <Image src={project.image} alt={project.title} /> */}
+              <Box>
+                <ImageGallery
+                  items={project.images as ReactImageGalleryItem[]}
+                  showPlayButton={false}
+                  showThumbnails={false}
+                />
+              </Box>
+
+              <Card.Body gap="2">
+                <Card.Title color={'secondary'}>{project.title}</Card.Title>
+                <Card.Description fontSize="16px" color={'secondaryText'}>
+                  {project.description}
+                </Card.Description>
+                <Box
+                  height={'100%'}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  justifyContent={'center'}
+                >
+                  <Box display="flex" gap="2" flexWrap={'wrap'} mt={3}>
+                    {project.tech_stacks.map((tech_stack: string, index: number) => (
+                      <Badge
+                        bg={'blue.700'}
+                        // colorPalette={'blue'}
+                        color={'secondaryText'}
+                        size={'md'}
+                        variant={'subtle'}
+                        key={index}
+                      >
+                        {tech_stack}
+                      </Badge>
+                    ))}
+                  </Box>
+                </Box>
+              </Card.Body>
+              <Card.Footer gap="2">
+                {project.github &&
+                  project.github.map((gh: Github, index: number) => (
+                    <Tag.Root
+                      key={index}
+                      size={'xl'}
+                      cursor={'pointer'}
+                      backgroundColor={'secondary'}
+                      color={'primaryText'}
+                      onClick={() => {
+                        if (gh) handleRedirect(gh.link);
+                      }}
+                    >
+                      <Tag.StartElement>
+                        <FaGithub />
+                      </Tag.StartElement>
+                      <Tag.Label>{gh.label}</Tag.Label>
+                    </Tag.Root>
+                  ))}
+                {project.website && (
+                  <Tag.Root
+                    size={'xl'}
+                    cursor={'pointer'}
+                    backgroundColor={'secondary'}
+                    color={'primaryText'}
+                    onClick={() => {
+                      if (project.website) handleRedirect(project.website);
+                    }}
+                  >
+                    <Tag.StartElement>
+                      <TbWorld />
+                    </Tag.StartElement>
+                    <Tag.Label>Website</Tag.Label>
+                  </Tag.Root>
+                )}
+              </Card.Footer>
+            </Card.Root>
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Portfolio;
---
+++ src/pages/test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import ImageGallery from 'react-image-gallery';
+
+const Test: React.FC = () => {
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
+  return (
+    <>
+      <Box>Test</Box>
+      <Box>
+        <ImageGallery items={images} />
+      </Box>
+    </>
+  );
+};
+
+export default Test;
